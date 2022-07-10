64ddac828da5c70d134fcf66ad6f3f08db2350a0
ibrahim4851 <erogluih2001@gmail.com>
2022-07-11 00:00:50 +0300

    ui update

--- app/src/main/java/com/ibrahim/nbafacts/util/Utils.kt
+++ app/src/main/java/com/ibrahim/nbafacts/util/Utils.kt
@@ -7,6 +7,7 @@ import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.bumptech.glide.request.RequestOptions
 import com.ibrahim.nbafacts.R
@@ -21,51 +22,47 @@ import io.reactivex.schedulers.Schedulers
 
 fun ImageView.downloadFromUrl(url: String?, progressDrawable: CircularProgressDrawable) {
     val options = RequestOptions()
+        .transform(RoundedCorners(55))
         .placeholder(progressDrawable)
         .error(R.mipmap.ic_launcher_round)
 
     Glide.with(context)
         .setDefaultRequestOptions(options)
         .load(url)
-        .centerCrop()
         .into(this)
-
 }
 
-fun placeHolderProgressBar(context: Context): CircularProgressDrawable{
+fun placeHolderProgressBar(context: Context): CircularProgressDrawable {
     return CircularProgressDrawable(context).apply {
         centerRadius = 40f
         strokeWidth = 8f
         start()
     }
 }
-/*
-fun <T> getJsonData(param: (String) -> Single<T>):Single<T>{
-    return NBAApiService::getPlayers
-}
-
-fun <T> BaseViewModel.getDataUtil( functionParameter: T, disposable: CompositeDisposable,
-                     apiFunction: (String) -> Single<T>){
-    //dataLoading.value = true
-    disposable.add(
-        apiFunction(functionParameter.toString())
-            .subscribeOn(Schedulers.newThread())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeWith(object : DisposableSingleObserver<T>(){
-                override fun onSuccess(t: T) {
-                    Log.i("utildata", t.toString())
-                }
-
-                override fun onError(e: Throwable) {
-                    //dataLoading.value = true
-                }
-
-            })
-    )
-}*/
 
 //below annotation makes it enable to use that function in xml files
 @BindingAdapter("android:downloadUrl")
-fun downloadImage(view: ImageView, url: String?){
+fun downloadImage(view: ImageView, url: String?) {
     view.downloadFromUrl(url, placeHolderProgressBar(view.context))
+}
+
+
+fun ImageView.downloadTeamLogo(abbreviation: String?, progressDrawable: CircularProgressDrawable) {
+    val url =
+        "https://i.cdn.turner.com/nba/nba/.element/img/1.0/teamsites/logos/teamlogos_500x500/" +
+                abbreviation?.lowercase() + ".png"
+    Log.i("resim", url)
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.mipmap.ic_launcher_round)
+
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(url)
+        .into(this)
+}
+
+@BindingAdapter("android:downloadOuterSource")
+fun downloadOuterSource(view: ImageView, abbreviation: String?) {
+    view.downloadTeamLogo(abbreviation, placeHolderProgressBar(view.context))
 }
